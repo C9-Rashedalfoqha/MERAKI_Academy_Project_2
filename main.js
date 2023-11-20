bd342cde697591689d1874bf417498f15d3737c5
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2023-11-20 23:49:03 +0300

    card style

--- main.js
+++ main.js
@@ -89,6 +89,136 @@ const product = [
     rate: 10,
     price: 25,
   },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },  {
+    id: 10,
+    title: "about product",
+    imageSrc:
+      "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "some description about product",
+    rate: 10,
+    price: 25,
+  },
 ];
 const getByBody = $("body");
 const navPar = $(`<div id="nav-par"><nav id="nav">
@@ -102,12 +232,14 @@ const navPar = $(`<div id="nav-par"><nav id="nav">
 </li><li><a href="#">HOME</a>
 </li></ul></nav></div>`);
 getByBody.append(navPar);
-
+const createParentDiv = $(`<div id="parent-div"></div>`);
 product.forEach((elem) => {
   const createProductDiv = $(`<div id="div-img"></div>`);
-  getByBody.append(createProductDiv);
-  const createImgTag = $(
-    `<img src="${elem.imageSrc}" alt=""><br/><p>${elem.description}</p>`);
-    createProductDiv.append(createImgTag);
 
+  const createImgTag = $(
+    `<tittle>${elem.title}</tittle><img src="${elem.imageSrc}" alt=""><br/><p>${elem.description}</p>`
+  );
+  createProductDiv.append(createImgTag);
+  getByBody.append(createParentDiv);
+  createParentDiv.append(createProductDiv);
 });
